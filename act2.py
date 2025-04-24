06cf8ce1d6f15b82bf03c7ab536eb69b565f27e1
sharan-0222 <sharanmenonc@gmail.com>
2025-04-24 19:28:15 +0530

    Add files via upload

---
+++ act2.py
@@ -0,0 +1,8 @@
+class car:
+    def __init__(self,mileage,range):
+        self.mileage=mileage
+        self.range=range
+
+modelx=car(22,9000)
+print("the mileage of this car is",modelx.mileage)
+print("the range of this car is",modelx.range)
